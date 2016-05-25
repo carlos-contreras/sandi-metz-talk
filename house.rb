5b810d57ceddc13123364eac9e10b77da604c809
Carlos Contreras <carlos.contreras@koombea.com>
2016-05-25 14:05:02 -0500

    Forth version

--- house.rb
+++ house.rb
@@ -1,4 +1,26 @@
 class House
+  DATA = [
+    'line 1',
+    'line 2',
+    'line 3',
+    'line 4',
+    'line 5',
+    'line 6',
+    'line 7',
+    'line 8',
+    'line 9',
+    'line 10',
+    'line 11',
+    'line 12',
+  ]
+
+  attr_reader :data, :formatter
+
+  def initialize(orderer: DefaultOrderer.new, formatter: DefaultFormatter.new)
+    @formatter = formatter
+    @data      = orderer.order(DATA)
+  end
+
   def recite
     (1..data.size).map { |i| line(i) }.join("\n")
   end
@@ -12,35 +34,30 @@ class House
   end
 
   def parts(number)
-    data.last(number)
-  end
-
-  def data
-    [
-      'line 1',
-      'line 2',
-      'line 3',
-      'line 4',
-      'line 5',
-      'line 6',
-      'line 7',
-      'line 8',
-      'line 9',
-      'line 10',
-      'line 11',
-      'line 12',
-    ]
+    formatter.format(data.last(number))
   end
 end
 
-class RandomHouse < House
-  def data
-    @data ||= super.shuffle
+class EchoFormatter
+  def format(parts)
+    parts.zip(parts).flatten
   end
 end
 
-class EchoHouse < House
-  def parts(number)
-    super.zip(super).flatten
+class DefaultFormatter
+  def format(parts)
+    parts
+  end
+end
+
+class RandomOrderer
+  def order(data)
+    data.shuffle
+  end
+end
+
+class DefaultOrderer
+  def order(data)
+    data
   end
 end
